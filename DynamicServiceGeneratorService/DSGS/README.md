49f79e69938d1cda2f517b2a66693a9d7961e566
Edet Ebenezer <segunebenezer72@gmail.com>
2023-04-30 16:10:26 -0500

    removed former DSGS files

--- DynamicServiceGeneratorService/DSGS/README.md
+++
@@ -1,2 +0,0 @@
-# DSGS
-Dynamic Service Generator Service
--- DynamicServiceGeneratorService/DSGS/src/main/resources/maven-resources/apache-maven-3.9.1/maven-model/src/main/java/org/apache/maven/model/package-info.java
+++
@@ -1,6 +0,0 @@
-// CHECKSTYLE_OFF: RegexpHeader
-/**
- * Maven POM (Project Object Model) classes, generated from <code>maven.mdo</code> model.
- * The root class is {@link org.apache.maven.model.Model}.
- */
-package org.apache.maven.model;
